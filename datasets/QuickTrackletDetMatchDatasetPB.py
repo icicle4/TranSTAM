ca542d5b10313400398257fbe3aa6aae5c7c3c0f
YiqiangFeng <yqfeng@aibee.com>
2022-03-08 16:15:07 +0800

    remove not use import

--- datasets/QuickTrackletDetMatchDatasetPB.py
+++ datasets/QuickTrackletDetMatchDatasetPB.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(__file__), "../.
 from datasets.memmory_bank_pb import MemoryBank
 import tqdm
 from datasets.warp_tracklet_and_detection_tensor import *
-from models.StrictSimpleCaseTracker import StrictSimpleCaseTracker, ImpossibleTracker
+from models.StrictSimpleCaseTracker import ImpossibleTracker
 from utils.metrics import AvgMetric
 
 
